b047a59272d7a75fb0604e7d9660b3a2b21c26c1
Keo Ponleou Sok <ponleousk@gmail.com>
2025-05-02 22:21:44 +1000

    feat: completed parse command functions

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #define LIGHT_PIN A0
 #define SERVO_PIN 12
 
-const String COMMAND_PREF = "ardmon";
+const String COMMAND_PREF = "ardmon ";
 
 Servo servo;
 
@@ -17,6 +17,11 @@ Writes into serial using prefixes:
   its logs are ":sound-detected,light-level" type: digital,analog
   its errors are "=error-message" type: string
 */
+
+void logData();
+void moveServo();
+void clearSerialBuffer();
+void parseCommand();
 
 void setup()
 {
@@ -30,19 +35,30 @@ void setup()
 void loop()
 {
   logData();
-  delay(10);
+  parseCommand();
+  delay(100);
 }
 
 void logData() {
   // log: ":sound-detected,light-level" type: digital,analog
   Serial.println(":" + String(digitalRead(SOUND_PIN)) + "," + String(analogRead(LIGHT_PIN)));
-  if (Serial.available()) {
-    Serial.println(Serial.read());
+}
+
+void clearSerialBuffer() {
+  while(Serial.available()) {
+    Serial.read(); // read to remove from buffer
   }
 }
 
+void moveServo() {
+  servo.write(30);
+  delay(100);
+  servo.write(0);
+  delay(100);
+}
+
 void parseCommand() {
-  int serial_index = 0;
+  unsigned int serial_index = 0;
   bool contain_prefix = false; // if command starts with "ardmon" set true
 
   const int MAX_FLAG_LENGTH = 64;
@@ -50,19 +66,35 @@ void parseCommand() {
   int flag_index = 0;
 
   while(Serial.available()) {
+    delay(10);
     char c = Serial.read();
 
-    // terminate on line break
-    if (c == '\n') {
+    // terminate on line break when its not a command 
+    if (c == '\n' && !contain_prefix) {
+      Serial.println("=Unknown Command");
       return;
     }
 
     // reading command flags
     if (contain_prefix) {
       
-      // if space or max index, process current flag
-      if (c == ' ' || flag_index < MAX_FLAG_LENGTH - 1) {
+      // command ended
+      if (c == '\n') {
+        contain_prefix = false;
+      }
+
+      // if space, line break, or max index, process current flag
+      if (c == ' ' || c == '\n' || !(flag_index < MAX_FLAG_LENGTH - 1)) {
         flag[flag_index] = '\0'; // terminate the string
+
+
+        if (strcmp(flag, "-ms") == 0) {
+          moveServo();
+        }
+        else {
+          Serial.println("=Unknown flag");
+        }
+
         flag_index = 0;
         continue;
       }
@@ -76,7 +108,7 @@ void parseCommand() {
 
     // check if line contains command prefix => read only if it is
     if (c == COMMAND_PREF[serial_index]) {
-      if(serial_index + 1 < COMMAND_PREF.length()) {
+      if(serial_index < COMMAND_PREF.length() - 1) {
         serial_index++;
         continue;
       }
@@ -91,15 +123,3 @@ void parseCommand() {
   }
 }
 
-void clearSerialBuffer() {
-  while(Serial.available()) {
-    Serial.read(); // read to remove from buffer
-  }
-}
-
-void moveServo() {
-  servo.write(30);
-  delay(10);
-  servo.write(0);
-  delay(100);
-}
